5f0c9ddafde918dfeac179de3e67fc308427cac3
zhengdao1 <2177164150@qq.com>
2026-09-08 21:06:50 +0800

    Restore correct Collatz from done with proj1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,15 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
-    
 
     public static void main(String[] args) {
         int n = 5;
